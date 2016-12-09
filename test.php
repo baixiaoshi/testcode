f225b734b0af469f45051005333afe29965f30ad
baixiaoshi <634842632@qq.com>
2016-12-10 07:51:33 +0800

    do commit

--- test.php
+++ test.php
@@ -5,7 +5,11 @@ abstract class Demo
 	protected static $name = 'baixiaoshi';
 
 
+<<<<<<< Updated upstream
 	public function get_name_fanglei()
+=======
+	public function get_name_hurong()
+>>>>>>> Stashed changes
 	{
 		echo static::$name;
         print_r("fanglei");
